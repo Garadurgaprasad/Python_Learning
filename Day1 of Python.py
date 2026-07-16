e15f431e11d0f4892004bf505d911d8c67972fae
Garadurgaprasad <151176941+Garadurgaprasad@users.noreply.github.com>
2026-07-16 12:43:03 +0530

    Day 1 of Learning Python
    What is Python, Why Python, Variables, Comments, Boolean Type, Swapping.

---
+++ Day1 of Python.py
@@ -0,0 +1,101 @@
+'''
+Day 1 of Learning Python
+------------------------
+Python 
+-in the year 1991 by Russom
+---------------------------
+What is Python?
+---------------------------
+high-level language
+oop
+Interpreted language 
+-code will be executed line by line that's the reason python is called interpreted language.
+
+input:
+num = 9
+num_2 = 10
+print(num)
+print(sum_2)
+
+output:
+9
+error
+
+Dynamically typed language
+-No need to mention the type of the data  passing to the variable
+
+num = 9
+print(type(num))
+
+it gives the output as "int"
+----------------------------
+Why Python ?
+
+-Easy to learn
+-Less syntax
+-easy to understand
+-open-source
+-large no of library
+----------------------------
+Comments:
+
+-sinle line (#)
+ This is used to explain in the code 
+ #example
+ 
+-multi line
+ This is used to Comment Multiple line using ''' ''' , """ """
+ '''example''' or """example"""
+-----------------------
+Variables:
+
+This is used to store the data types
+Good way to define: 
+	start with _			ex: _num
+	start with alphabets	ex: num or Num
+	
+Bad way to Define:
+	can't use with special character.		ex:@num or &py or !num
+	can't start with number					ex: 1num or num1 or n1m
+	can't use space							ex: num one or num 2
+-----------------------------------
+Boolean Type:
+-it prints either True or False
+
+num=9
+num_2=89
+print(num != num_2)
+
+output:
+True
+----------------
+num = 78
+num = 88
+print(num)
+
+output: 88				#over write in python
+-------------------
+num = num_2 = num_3 = 78	#multiple variables-sinle value
+Print(num_2)
+
+output:78
+-----------------------
+num, num_2 = 9,90		#multiple variables-multiple values
+print(num)
+print(num_2)
+
+output:
+9
+90
+------------------------------------
+swapping of two values:
+
+num = 7
+num_2 = 70
+num , num_2 = num_2 , num		#7 , 70 = 70 , 7
+print(num)
+
+output:
+70
+-------------------------------
+'''
